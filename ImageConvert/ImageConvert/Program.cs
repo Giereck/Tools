b47f90ff307ce7bbc4a83a0dc64eaa48435193af
Kristiansen Andreas (DFDS A/S) <andkr@DFDS.COM>
2017-03-02 13:45:40 +0100

    Changed value to 3

--- ImageConvert/ImageConvert/Program.cs
+++ ImageConvert/ImageConvert/Program.cs
@@ -35,7 +35,7 @@ namespace ImageConvert
                 }                                   
             }
 
-            Console.WriteLine("All done2!");
+            Console.WriteLine("All done3!");
             Console.ReadKey();            
         }
     }
